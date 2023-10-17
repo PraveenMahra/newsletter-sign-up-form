3bd32b45447175b0ab091ea4e220cffd4344bf9a
Praveen singh mahra <Praveen.mehra65@gmail.com>
2023-10-17 17:11:11 +0530

    add email validator

---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.querySelector("#email");
+const errorMessage = document.querySelector(".error-message");
+const form = document.querySelector("form");
+
+function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return emailPattern.test(email);
+}
+
+function emailValidator() {
+  const email = input.value;
+
+  if (email === "") {
+    input.classList.remove("input-error");
+    errorMessage.classList.add("hidden");
+    return false;
+  }
+
+  if (!validateEmail(email)) {
+    input.classList.add("input-error");
+    errorMessage.classList.remove("hidden");
+    return false;
+  }
+
+  input.classList.remove("input-error");
+  errorMessage.classList.add("hidden");
+  return true;
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const isValidEmail = emailValidator();
+
+  if (isValidEmail) window.location = "/success.html";
+});
